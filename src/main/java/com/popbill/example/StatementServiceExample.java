8bc9e23c3dcd2ba6ff4d5c3d24b3414169d8b70a
john <john@linkhub.co.kr>
2021-04-01 17:41:45 +0900

    fixed updatedt.

--- src/main/java/com/popbill/example/StatementServiceExample.java
+++ src/main/java/com/popbill/example/StatementServiceExample.java
@@ -2,7 +2,7 @@
  * 팝빌 전자명세서 API Java SDK SpringMVC Example
  *
  * - SpringMVC SDK 연동환경 설정방법 안내 : https://docs.popbill.com/statement/tutorial/java
- * - 업데이트 일자 : 2020-01-20
+ * - 업데이트 일자 : 2021-04-01
  * - 연동 기술지원 연락처 : 1600-9854 / 070-4304-2991~2
  * - 연동 기술지원 이메일 : code@linkhub.co.kr
  *
