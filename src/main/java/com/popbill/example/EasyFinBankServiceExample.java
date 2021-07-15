e5876b20aedd1ace34093bcb0497fa5eeb1d110f
ProWON <wjkim@linkhub.co.kr>
2021-07-15 13:28:13 +0900

    added EasyFin listActiveJob comment

--- src/main/java/com/popbill/example/EasyFinBankServiceExample.java
+++ src/main/java/com/popbill/example/EasyFinBankServiceExample.java
@@ -403,6 +403,7 @@ public class EasyFinBankServiceExample {
     public String listActiveJob(Model m) {
         /*
          * RequestJob(수집 요청)를 통해 반환 받은 작업아이디의 목록을 확인합니다.
+         * - 수집 요청 후 1시간이 경과한 수집 요청건은 상태정보가 반환되지 않습니다.
          * - https://docs.popbill.com/easyfinbank/java/api#ListActiveJob
          */
 
