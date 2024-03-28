1f88fab12633e51d008e057495db8a5cccfe61f1
hyeseon <hyeseon@linkhubcorp.com>
2024-03-28 11:49:35 +0900

    registTaxCert, registTaxCertPFX 주석 추가

--- src/main/java/com/popbill/example/TaxinvoiceServiceExample.java
+++ src/main/java/com/popbill/example/TaxinvoiceServiceExample.java
@@ -4899,6 +4899,15 @@ public class TaxinvoiceServiceExample {
 
     @RequestMapping(value = "registTaxCert", method = RequestMethod.GET)
     public String registTaxCert(Model m) {
+        /**
+         * 전자세금계산서 발행에 필요한 공동인증서를 팝빌 인증서버에 등록합니다.
+         * 공동인증서는 팝빌에서 발급하는 '표준 인증서'와 '범용 인증서', 은행에서 발급하는 '전자세금용 인증서'만 등록이 가능합니다.
+         * 공동인증서 정보는 통신 구간의 보안을 위해 필드 레벨 암호화(FLE)되어 처리됩니다.
+         * 통신구간 암호화 키 발급은 파트너 센터(1600-8536)로 문의하여 주시기 바랍니다.
+         * 공동인증서 비밀번호의 안전한 관리를 위해 DB 저장시 컬럼 암호화 하여 저장합니다.
+         * - https://developers.popbill.com/reference/taxinvoice/java/api/cert#RegistTaxCert
+         */
+
         try {
 
             // 공동인증서 공개키 (Base64 Encoded)
@@ -4921,8 +4930,16 @@ public class TaxinvoiceServiceExample {
 
     @RequestMapping(value = "registTaxCertPFX", method = RequestMethod.GET)
     public String registTaxCertPFX(Model m) {
-        try {
+        /**
+         * 전자세금계산서 발행에 필요한 공동인증서를 팝빌 인증서버에 등록합니다.
+         * 공동인증서는 팝빌에서 발급하는 '표준 인증서'와 '범용 인증서', 은행에서 발급하는 '전자세금용 인증서'만 등록이 가능합니다.
+         * 공동인증서 정보는 통신 구간의 보안을 위해 필드 레벨 암호화(FLE)되어 처리됩니다.
+         * 통신구간 암호화 키 발급은 파트너 센터(1600-8536)로 문의하여 주시기 바랍니다.
+         * 공동인증서 비밀번호의 안전한 관리를 위해 DB 저장시 컬럼 암호화 하여 저장합니다.
+         * - https://developers.popbill.com/reference/taxinvoice/java/api/cert#RegistTaxCertPFX
+         */
 
+        try {
 
             // 공동인증서 PFX 파일 (Base64 Encoded)
             String PFX = "";
